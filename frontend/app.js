064577d55050a4641eae3ab9852e741797d0af6c
SocratesMindset <artur251373@gmail.com>
2026-05-26 18:51:23 +0300

    feat: new stat blocks added with global terminal window

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,7 @@
 // app.js
+var backendAPI = "http://localhost:8080/api";
 async function loadStats() {
-  const cpu = await fetch("http://localhost:8080/api/test")
+  const cpu = await fetch(backendAPI + "/test")
     .then((r) => r.json())
     .then(() => {
       document.getElementById("cpu").textContent = "api test successful";
@@ -8,10 +9,18 @@ async function loadStats() {
 }
 
 async function loadTemp() {
-  const temperature = fetch("http://localhost:8080/api/temperature")
+  await fetch(backendAPI + "/temperature")
     .then((r) => r.json())
     .then((data) => {
       document.getElementById("templb").textContent = data.temperature;
+    });
+}
+
+async function loadPayload() {
+  const payLoad = fetch(backendAPI + "/payload")
+    .then((res) => res.json())
+    .then((data) => {
+      document.getElementById("payLoad").textContent = data.payload;
     });
 }
 document.getElementById("refresh").addEventListener("click", loadStats);
